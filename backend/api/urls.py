5b23347bc1905904ba3a89d927a9af4699450ff7
Вячеслав Клепалов <slavaklepalov@gmail.com>
2023-06-01 21:19:24 +0500

    Фикс багов с редок.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -17,9 +17,4 @@ router.register('users', CustomUserViewSet, 'users')
 urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('djoser.urls.authtoken')),
-    path(
-        'docs/',
-        TemplateView.as_view(template_name='redoc.html'),
-        name='docs'
-    ),
 ]
--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -71,11 +71,10 @@ DJOSER = {
 
 ROOT_URLCONF = 'foodgram.urls'
 
-TEMPLATES_DIR = BASE_DIR / 'templates'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
